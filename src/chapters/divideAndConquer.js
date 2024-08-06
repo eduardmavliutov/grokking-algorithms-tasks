bcb27d2e2b5b04d7b0051bbb45de16d768bd4295
Eduard Mavliutov <fox11-best@yandex.ru>
2024-08-06 12:02:58 +0200

    implement quick sort

--- src/chapters/divideAndConquer.js
+++ src/chapters/divideAndConquer.js
@@ -7,3 +7,25 @@ export const sumArrayRecursively = (arr) => {
 
   return arr[0] + sumArrayRecursively(arr.slice(1))
 }
+
+export const quickSort = (arr) => {
+  if (arr.length < 2) {
+    return arr
+  } else if (arr.length === 2) {
+    return arr[0] <= arr[1] ? arr : [arr[1], arr[0]]
+  }
+
+  const pivot = arr[0]
+  const less = []
+  const greater = []
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      less.push(arr[i])
+    } else {
+      greater.push(arr[i])
+    }  
+  }
+
+  return [...quickSort(less), pivot, ...quickSort(greater)]
+}
--- src/chapters/divideAndConquer.test.js
+++ src/chapters/divideAndConquer.test.js
@@ -1,5 +1,5 @@
 import { expect, describe, test } from 'vitest';
-import { sumArrayRecursively } from './divideAndConquer';
+import { sumArrayRecursively, quickSort } from './divideAndConquer';
 
 describe('devide and conquer tests', () => {
   test('sumArrayRecursively returns 0 for an empty array', () => {
@@ -16,5 +16,17 @@ describe('devide and conquer tests', () => {
 
   test('sumArrayRecursively counts sum properly for', () => {
     expect(sumArrayRecursively([1, 2, 3, 4])).toBe(10)
+  })
+
+  test('quickSort returns the same array if array contains only 1 item', () => {
+    expect(quickSort([33])).toEqual([33])
+  })
+
+  test('quickSort sorts an array that contains 2 items', () => {
+    expect(quickSort([33, 11])).toEqual([11, 33])
+  })
+
+  test('quickSort sorts an array', () => {
+    expect(quickSort([33, 11, 3, 4, 99, 415, 43])).toEqual([3, 4, 11, 33, 43, 99, 415])
   })
 })
