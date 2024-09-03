a2f5027ee14c60d938a0f7a68eacc37cace80614
Eduard Mavliutov <fox11-best@yandex.ru>
2024-09-03 15:44:09 +0200

    implement recursive getter for fibonachi numbers

--- src/chapters/recursion.js
+++ src/chapters/recursion.js
@@ -13,3 +13,20 @@ export const raiseToPower = (num, power) => {
 
   return num * raiseToPower(num, power - 1)
 }
+
+export const getFibonachiNumber = (num) => {
+  if (num === 0 || num === 1) {
+    return 1
+  }
+
+  return getFibonachiNumber(num - 1) + getFibonachiNumber(num - 2)
+}
+
+export const getFibonachiNumberWithMemo = (num, previousNumbers = [1, 1]) => {
+  const fibonachiNumbers = [...previousNumbers]
+  if (!fibonachiNumbers[num]) {
+    fibonachiNumbers[num] = getFibonachiNumberWithMemo(num - 1, fibonachiNumbers) + getFibonachiNumberWithMemo(num - 2, fibonachiNumbers)
+  }
+
+  return fibonachiNumbers[num]
+}
--- src/chapters/recursion.test.js
+++ src/chapters/recursion.test.js
@@ -1,22 +1,45 @@
-import { factorial, raiseToPower } from './recursion'
+import { factorial, raiseToPower, getFibonachiNumber, getFibonachiNumberWithMemo } from './recursion'
 import { expect, test, describe } from 'vitest'
 
 describe('recursion tests', () => {
-  test('factorial of 5 equals 120', () => {
-    const result = factorial(5)
-    expect(result).toBe(120)
+
+  describe('factorial', () => {
+    test('factorial of 5 equals 120', () => {
+      const result = factorial(5)
+      expect(result).toBe(120)
+    })
+    
+    test('factorial of 1 equals 1', () => {
+      const result = factorial(1)
+      expect(result).toBe(1)
+    })
   })
+
+  describe('raise to power', () => {
+    test('2 in the third power equals 8', () => {
+      expect(raiseToPower(2, 3)).toBe(8)
+    })
   
-  test('factorial of 1 equals 1', () => {
-    const result = factorial(1)
-    expect(result).toBe(1)
+    test('number in the zeroth power equals 1', () => {
+      expect(raiseToPower(3, 0)).toBe(1)
+    })
   })
 
-  test('2 in the third power equals 8', () => {
-    expect(raiseToPower(2, 3)).toBe(8)
-  })
+  describe('fibonachi', () => {
+    test('return 1 for the zeroth fibonachi number', () => {
+      expect(getFibonachiNumber(0)).toBe(1)
+    })
 
-  test('number in the zeroth power equals 1', () => {
-    expect(raiseToPower(3, 0)).toBe(1)
+    test('return 1 for the first fibonachi number', () => {
+      expect(getFibonachiNumber(0)).toBe(1)
+    })
+
+    test('return 5 for the fifth fibonachi number', () => {
+      expect(getFibonachiNumber(4)).toBe(5)
+    })
+
+    test('return 5 for the fifth fibonachi number', () => {
+      expect(getFibonachiNumberWithMemo(4)).toBe(5)
+    })
   })
 })
